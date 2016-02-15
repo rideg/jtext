9ba4747ccd170e070c2539ad9efd01c9e40bff40
Sandor Rideg <sandor.rideg@gmail.com>
2016-02-15 17:11:56 +0100

    [rideg] more tests on LinearLayout

--- src/main/java/org/jtext/ui/layout/LinearLayout.java
+++ src/main/java/org/jtext/ui/layout/LinearLayout.java
@@ -31,6 +31,10 @@ public class LinearLayout extends Layout {
         final List<Widget> visibleWidgets = widgetOrder.stream().filter(Widget::isVisible).collect(Collectors.toList());
         widgetOrder.stream().filter(w -> !w.isVisible()).forEach(w -> widgets.put(w, Rectangle.empty()));
 
+        if (visibleWidgets.isEmpty()) {
+            return;
+        }
+
         int remaining = dimension.width;
         int startX = 0;
         int numberOfFillingWidgets = 0;
@@ -40,7 +44,10 @@ public class LinearLayout extends Layout {
                 final Margin margin = widget.getMargin();
                 int realWidth = widget.getPreferredWidth().toReal(dimension.width);
                 int realHeight = widget.getRealHeight(dimension.height);
-                widgets.put(widget, Rectangle.of(startX + margin.left, margin.top, realWidth, realHeight));
+
+                int startY = getStartY(realHeight, margin);
+
+                widgets.put(widget, Rectangle.of(startX + margin.left, startY, realWidth, realHeight));
                 remaining -= realWidth + margin.horizontalSpacing();
                 startX += realWidth + margin.horizontalSpacing();
             } else {
@@ -62,7 +69,8 @@ public class LinearLayout extends Layout {
                     }
                     int realHeight = widget.getRealHeight(dimension.height);
                     final Margin margin = widget.getMargin();
-                    widgets.put(widget, Rectangle.of(startX + margin.left, margin.top, width, realHeight));
+                    int startY = getStartY(realHeight, margin);
+                    widgets.put(widget, Rectangle.of(startX + margin.left, startY, width, realHeight));
                     startX += width + margin.horizontalSpacing();
                     shift += width + margin.horizontalSpacing();
                 } else {
@@ -72,12 +80,42 @@ public class LinearLayout extends Layout {
                 }
             }
         } else if (horizontalAlign != HorizontalAlign.LEFT && remaining > 0) {
-            final int shift = horizontalAlign == HorizontalAlign.CENTER ? remaining / 2 : remaining;
-            visibleWidgets
-                    .stream()
-                    .filter(Widget::isVisible)
-                    .forEach(w -> widgets.put(w, widgets.get(w).shiftX(shift)));
+            final Widget firstWidget = visibleWidgets.get(0);
+            final Widget lastWidget = visibleWidgets.get(visibleWidgets.size() - 1);
+
+            remaining += firstWidget.getMargin().left;
+            remaining += lastWidget.getMargin().right;
+
+            int shift = horizontalAlign == HorizontalAlign.CENTER ? remaining / 2 : remaining;
+
+            if (shift < firstWidget.getMargin().left && horizontalAlign == HorizontalAlign.CENTER) {
+                shift = firstWidget.getMargin().left;
+            }
+
+            if (horizontalAlign == HorizontalAlign.RIGHT) {
+                shift -= lastWidget.getMargin().right;
+            }
+
+            for (final Widget widget : visibleWidgets) {
+                final int tmpShift = widget == firstWidget ? shift - widget.getMargin().left : shift;
+                widgets.put(widget, widgets.get(widget).shiftX(tmpShift));
+            }
         }
     }
 
+    private int getStartY(final int height, final Margin margin) {
+        int ret = margin.top;
+        if (verticalAlign != VerticalAlign.TOP) {
+            int remaining = dimension.height - height;
+            ret = verticalAlign == VerticalAlign.CENTER ? remaining / 2 : remaining;
+
+            if (ret < margin.top && verticalAlign == VerticalAlign.CENTER) {
+                ret = margin.top;
+            }
+            if (verticalAlign == VerticalAlign.BOTTOM) {
+                ret -= margin.bottom;
+            }
+        }
+        return ret;
+    }
 }
--- src/test/java/org/jtext/ui/layout/LinearLayoutTest.java
+++ src/test/java/org/jtext/ui/layout/LinearLayoutTest.java
@@ -4,6 +4,7 @@ import org.hamcrest.core.Is;
 import org.jtext.testsupport.DummyWidget;
 import org.jtext.ui.attribute.HorizontalAlign;
 import org.jtext.ui.attribute.Margin;
+import org.jtext.ui.attribute.VerticalAlign;
 import org.jtext.ui.graphics.Dimension;
 import org.jtext.ui.graphics.Position;
 import org.jtext.ui.graphics.Rectangle;
@@ -48,6 +49,19 @@ public class LinearLayoutTest {
     private static final DummyWidget PROPORTIONAL_WIDGET = new DummyWidget(percent(30), percent(50), fixed(5),
                                                                            fixed(5), percent(30), percent(50),
                                                                            Position.RELATIVE, true);
+
+
+    private static final DummyWidget FIXED_SMALL_WIDGET_MARGIN = new DummyWidget(fixed(1), fixed(1), fixed(1),
+                                                                           fixed(1), fixed(1), fixed(1),
+                                                                           Margin.of(0, 3, 0, 1),
+                                                                           Position.RELATIVE);
+
+
+    private static final DummyWidget FIXED_SMALL_WIDGET_MARGIN_MIN_SIZE = new DummyWidget(fixed(2), fixed(1), fixed(1),
+                                                                                          fixed(1), fixed(1), fixed(1),
+                                                                                          Margin.of(0, 3, 0, 1),
+                                                                                          Position.RELATIVE);
+
     private LinearLayout layout;
 
     @Test
@@ -164,6 +178,40 @@ public class LinearLayoutTest {
         Assert.assertThat(area, Is.is(Rectangle.of(0, 0, 4, 6)));
     }
 
+    @Ignore
+    @Test
+    public void shouldUseMinWidthIfAvailableSpaceIsLess() throws Exception {
+
+        // given
+        layout = Layouts.horizontal(HorizontalAlign.LEFT);
+        layout.addWidget(FIXED_WIDGET);
+
+        // when
+        layout.setDimension(Dimension.of(2, 6));
+
+        // then
+        final Rectangle area = layout.getAreaFor(FIXED_WIDGET);
+        Assert.assertThat(area, Is.is(Rectangle.of(0, 0, 3, 6)));
+    }
+
+    @Test
+    public void shouldAlignVerticallyAndConsideringMarginToo() throws Exception {
+        // given
+        layout = Layouts.horizontal(HorizontalAlign.LEFT, VerticalAlign.BOTTOM);
+        layout.addWidget(FIXED_WIDGET_MARGIN);
+        layout.addWidget(FIXED_WIDGET_2);
+
+        // when
+        layout.setDimension(Dimension.of(50, 25));
+
+        // then
+        final Rectangle area1 = layout.getAreaFor(FIXED_WIDGET_MARGIN);
+        final Rectangle area2 = layout.getAreaFor(FIXED_WIDGET_2);
+        final Margin margin = FIXED_WIDGET_MARGIN.getMargin();
+        Assert.assertThat(area1, Is.is(Rectangle.of(margin.left, 19 - margin.bottom, 6, 6)));
+        Assert.assertThat(area2, Is.is(Rectangle.of(6 + margin.left + margin.right, 21, 4, 4)));
+    }
+
     @Test
     public void shouldConsiderMinHeight() throws Exception {
         // given
@@ -224,6 +272,35 @@ public class LinearLayoutTest {
         final Margin margin = FIXED_WIDGET_MARGIN.getMargin();
         Assert.assertThat(area1, Is.is(Rectangle.of(margin.left, margin.top, 6, 6)));
         Assert.assertThat(area2, Is.is(Rectangle.of(6 + margin.left + margin.right, 0, 4, 4)));
+    }
+
+    @Test
+    public void shouldConsiderMarginProperlyWhenCentering() throws Exception {
+        // given
+        layout = Layouts.horizontal(HorizontalAlign.CENTER);
+        layout.addWidget(FIXED_SMALL_WIDGET_MARGIN);
+
+        // when
+        layout.setDimension(Dimension.of(7, 1));
+
+        // then
+        final Rectangle area = layout.getAreaFor(FIXED_SMALL_WIDGET_MARGIN);
+        Assert.assertThat(area, Is.is(Rectangle.of(3, 0, 1, 1)));
+    }
+
+    @Ignore
+    @Test
+    public void shouldConsiderGiveLessSpaceForWidgetWithMarginToFulfillMarginRequirements() throws Exception {
+        // given
+        layout = Layouts.horizontal(HorizontalAlign.RIGHT);
+        layout.addWidget(FIXED_SMALL_WIDGET_MARGIN_MIN_SIZE);
+
+        // when
+        layout.setDimension(Dimension.of(5, 1));
+
+        // then
+        final Rectangle area = layout.getAreaFor(FIXED_SMALL_WIDGET_MARGIN_MIN_SIZE);
+        Assert.assertThat(area, Is.is(Rectangle.of(0, 0, 1, 1)));
     }
 
     @Test
